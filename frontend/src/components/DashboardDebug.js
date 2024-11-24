118d725f7f0f3c1e8f105cdbf75e712256c2e893
kedarvyas <kedar.m.vyas@gmail.com>
2024-11-23 17:10:22 -0700

    dashboard debug2

--- frontend/src/components/DashboardDebug.js
+++ frontend/src/components/DashboardDebug.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from '@/components/ui/card';
+import { Card } from "./ui/card";
 
 const DashboardDebug = () => {
   const [apiStatus, setApiStatus] = React.useState({
